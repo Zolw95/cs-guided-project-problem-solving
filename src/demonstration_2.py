69271ebee0f1e14f9f198e2d225b67b8676228d9
unknown <miloszz87@gmail.com>
2020-10-02 23:19:22 -0400

    Working on python problems

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -30,6 +30,32 @@ Return:
     "LambdaSchool"
 ]
 """
-def lambda_school(n):
-    # Your code here
 
+
+def lambda_school(n):
+    arr = []
+    # Your code here
+    # Create a list that takes in an integer from index + 1 upto n
+    for i in range(1, n + 1):
+        arr.append(i)
+    # It should RETURN an ARRAY of strings up to that integer - starting from 1
+    # return arr
+    # If the number at specific index is a multiple of 3 - it should output Lambda
+    for i, item in enumerate(arr):
+        print("ARR", arr)
+        if (item % 3 == 0 and item % 5 == 0):
+            arr[i] = "LambdaSchool"
+        elif (item % 5 == 0):
+            arr[i] = "School"
+        elif (item % 3 == 0):
+            arr[i] = "Lambda"
+        print(arr)
+    # for i in range(1, len(arr)):
+    #     print("I", i)
+    #     if i % 3 == 0:
+    #         arr[i] = "School"
+
+result = lambda_school(15)
+    # If the number at specific index is a multiple of 5 - it should output Lambda
+    # if the number at specific index is a multiple of 3 AND 5 - it should output LambdaSchool
+    
